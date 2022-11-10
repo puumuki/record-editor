b32b7396913db66d25c680fed4b7fd5e4a11a7b0
Teemu Puukko <teemuki@gmail.com>
2022-11-10 20:13:31 +0200

    Disable SSL reject unauthorized

--- lib/race-recorder/postgresql-connection.ts
+++ lib/race-recorder/postgresql-connection.ts
@@ -3,7 +3,12 @@ import { Pool } from "pg";
 let connection:Pool;
 
 if( process.env.DATABASE_URL ) {
-  connection = new Pool({ connectionString: process.env.DATABASE_URL });  
+  connection = new Pool({ 
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });  
 } else {
   connection = new Pool({
     user: process.env.PGSQL_USER,
@@ -11,6 +16,9 @@ if( process.env.DATABASE_URL ) {
     host: process.env.PGSQL_HOST,
     port: parseInt(process.env.PGSQL_PORT!),
     database: process.env.PGSQL_DATABASE,
+    ssl: {
+      rejectUnauthorized: false
+    }
   });  
 }
 
