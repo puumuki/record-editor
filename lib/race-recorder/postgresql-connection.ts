9ea490880946e46bc63a88709cc1a9b42f89c5f2
Teemu Puukko <teemuki@gmail.com>
2022-11-10 10:12:19 +0200

    Trying add database connection to production enviroment

--- lib/race-recorder/postgresql-connection.ts
+++ lib/race-recorder/postgresql-connection.ts
@@ -1,11 +1,17 @@
 import { Pool } from "pg";
 
-const connection = new Pool({
-  user: process.env.PGSQL_USER,
-  password: process.env.PGSQL_PASSWORD,
-  host: process.env.PGSQL_HOST,
-  port: parseInt(process.env.PGSQL_PORT!),
-  database: process.env.PGSQL_DATABASE,
-});
+let connection:Pool;
+
+if( process.env.PSQL_CONNECT_STRING ) {
+  connection = new Pool({ connectionString: process.env.PSQL_CONNECT_STRING });  
+} else {
+  connection = new Pool({
+    user: process.env.PGSQL_USER,
+    password: process.env.PGSQL_PASSWORD,
+    host: process.env.PGSQL_HOST,
+    port: parseInt(process.env.PGSQL_PORT!),
+    database: process.env.PGSQL_DATABASE,
+  });  
+}
 
 export default connection;
