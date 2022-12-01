aa7564bc18182bc7832b07f1d3142cb668ad0f42
Teemu Puukko <teemuki@gmail.com>
2022-12-01 23:02:42 +0200

    Added statistics to RecordEditorView

---
+++ components/TimeView.tsx
@@ -0,0 +1,13 @@
+
+interface TimeViewProps {
+  seconds: number
+}
+
+const TimeView = (props:TimeViewProps) => {  
+  const hours = Math.floor(props.seconds / 3600);
+  const minutes = Math.floor(props.seconds % 3600/ 60);
+  const seconds = Math.floor(props.seconds % 60);
+  return <span>{hours}h {minutes}min {seconds}s</span>
+}
+
+export default TimeView;
