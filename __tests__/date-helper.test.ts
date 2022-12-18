089f4dbfc190d20894d79955b94c8bfbb5dc9d31
Teemu Puukko <teemuki@gmail.com>
2022-12-18 19:31:57 +0200

    Changed from Dayjs to Moment.js

--- __tests__/date-helper.test.ts
+++ __tests__/date-helper.test.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from 'vitest'
 import { unixTimeToLocalTime, dateTimeSeparated, dateTimeSeparatedToUnixTime } from '../lib/date-helper'
 
 describe('Convert seconds to minutes, seconds and hurdreds of second.', () => {
+
   it('dateTimeSeparated()', () => {
     const result = dateTimeSeparated(1667543808183)
     expect(result.date).toEqual('2022-11-04')
@@ -9,11 +10,11 @@ describe('Convert seconds to minutes, seconds and hurdreds of second.', () => {
   })
 
   it("dateTimeSeparatedToUnixTime({ date: '2022-11-04', time: '08:36' })", () => {
-    const separatedTime = { date: '2022-11-04', time: '08:36' }
+    const separatedTime = { date: '2022-12-18', time: '18:55' }
     const result = dateTimeSeparatedToUnixTime(separatedTime)
 
     // Some precision are lost due we don't include milliseconds, notice zeros at the end.
-    expect(result).toEqual(1667543760000)
+    expect(result).toEqual(1671382500000)
 
     // But converting back to separated time should result same result
     const separatedTime2 = dateTimeSeparated(result)
@@ -23,7 +24,7 @@ describe('Convert seconds to minutes, seconds and hurdreds of second.', () => {
   })
 
   it('Convert unix time to localtime', () => {
-    const result = unixTimeToLocalTime(1667543760000)
-    expect(result).toEqual('2022-11-04 08:36')
+    const result = unixTimeToLocalTime(1671382500000)
+    expect(result).toEqual('2022-12-18 18:55')
   })
 })
